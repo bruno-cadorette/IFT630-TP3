8b5c23264e1737002c929ce089f4d8e4258767e1
Bruno Cadorette <brunocadorette@outlook.com>
2015-07-21 16:54:54 -0400

    Pion + Cavalier

--- ChessRules.hs
+++ ChessRules.hs
@@ -1,4 +1,6 @@
 import qualified Data.Map as Map
+
+
 type Board = Map.Map (Int,Int) ChessPiece
 
 class (Show p)=>Piece p where
@@ -6,6 +8,9 @@ class (Show p)=>Piece p where
     movement::p->(Int,Int)->Board->[(Int,Int)]
     
 data PieceColor = White | Black deriving(Eq,Show)
+enemyColor White = Black
+enemyColor Black = White
+
 data PieceType = King | Queen | Rook | Bishop | Knight | Pawn deriving(Show)
 
 data ChessPiece = ChessPiece {
@@ -13,7 +18,18 @@ data ChessPiece = ChessPiece {
     pieceColor :: PieceColor
 } deriving(Show)
 
+data ChessGame = ChessGame PieceColor Board
 
+instance Piece ChessPiece where
+    value (ChessPiece t _)= pieceValue t
+    movement = movementImpl
+
+transitionImpl (ChessGame color board) = 
+    concatMap transitions $ filter (\(k,x)->pieceColor x == color) $ Map.toList  board
+    where
+        transitions (pos, elem) = map (\t->ChessGame (enemyColor c) $ rebuild t) $ movement elem pos board
+            where
+                rebuild target = Map.insert target elem $ Map.delete pos board
 pieceValue King = 1000
 pieceValue Queen = 9
 pieceValue Rook = 5
@@ -31,16 +47,17 @@ movementImpl (ChessPiece t color) xy game = movementKind xy color game
                 Bishop -> movementBishop
                 Knight -> movementKnight
                 Pawn -> movementPawn
-{-
-instance Piece ChessPiece where
-    value (ChessPiece t _)= pieceValue t
-    movement = movementImpl
-    -}
+
+    
 borderMin = max 0
 borderMax = min 7
 
 isOutOfMap (x,y) = x >= 8 || y >= 8 || x < 0 || y < 0
-
+canEat pos myColor board = 
+    case Map.lookup pos board of 
+        Just(x) -> pieceColor x /= myColor
+        Nothing -> True
+        
 block color board = block' 
     where 
         block' [] = []
@@ -55,14 +72,11 @@ block color board = block'
                             []
                     Nothing->pos:block' xs
 
-movementKing (x,y) c g = filter (\(i,j)-> (not (isOutOfMap (i,j))) && (not (x==i && y == j)) && canEat (i,j) c g) possibleMovements
+movementKing (x,y) c g = filter (\(i,j)-> (not (isOutOfMap (i,j))) && (not (x == i && y == j)) && canEat (i,j) c g) possibleMovements
     where
         possibleMovements = 
             [(i,j)|i<-[(borderMin x-1)..(borderMax x+1)], j<-[(borderMin y-1)..(borderMax y+1)]]
-        canEat pos myColor board = 
-            case Map.lookup pos board of 
-                Just(x) -> pieceColor x /= myColor
-                Nothing -> True
+        
         
 movementQueen pos c g = concatMap (\f-> f pos c g) [movementRook, movementBishop]
                 
@@ -72,6 +86,7 @@ movementRook (x,y) c g = up ++ down ++ right ++ left
         down = block c g [(x,i)|i<-[borderMin (y-1), borderMin (y-2) .. ]]
         right = block c g [(i,y)|i<-[borderMax (x+1) .. ]]
         left = block c g [(i,y)|i<-[borderMin (x-1), borderMin (x-2) .. ]]
+            
         
 movementBishop (x,y) c g = 
     (generator (\(x,y)->(x+1,y+1))) ++ 
@@ -81,18 +96,24 @@ movementBishop (x,y) c g =
     where
         generator f = block c g $ tail $ iterate f (x,y)
         
-movementKnight a c g = []
-
-movementPawn a b c = []
-{-
-movementPawn White (x,y) game = canEat (x+1,y+1) White [(x-1,y+1)] ++ (if y == 1 then [(x,y+2)] else [])
+movementKnight (x,y) c g = filter(\pos->not (isOutOfMap pos) || canEat pos c g ) $ map (\(i,j)->(x+i,y+j)) possible
     where
-        canEat pos color = 
-            case Map.lookup pos game of
-                Just (ChessPiece t c) -> c /= color
-                Nothing ->[]
-        
-movementPawn Black (x,y) game = 
-    if (y == 6) then [(x,y-2)]
-    else [(x,y-1)]
--}
+        possible = [(-2,-1),(-2,1),(-1,-2),(-1,2),(1,-2),(1,2),(2,-1),(2,1)]
+movementPawn (x,y) c g = filter (\pos->not $ isOutOfMap pos) $ [(x,y+d)] ++ pawnEat ++ firstMove
+    where
+    pawnEat = filter pawnEat' [(x-1,y + d),(x+1,y+d)]
+        where 
+        pawnEat' pos =
+            case Map.lookup pos g of 
+                Just(x)->pieceColor x /= c
+                Nothing->False
+    firstMove = if fm then [(x,y+2*d)] else []
+        where 
+        fm =
+            case c of
+                White->y==1
+                Black->y==6
+    d = 
+        case c of
+            White-> 1
+            Black-> -1
