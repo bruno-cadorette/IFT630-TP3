800138df40d4a20c90848d7e0be6e6385d2ad846
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2015-07-28 15:16:04 -0400

    Fix MPI, on peut pas envoyer de Data facilement donc on va shipper des Tuples

--- MPIServer.hs
+++ MPIServer.hs
@@ -1,31 +1,37 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module MPIServer where
 
 import Control.Parallel.MPI.Simple (mpiWorld, commWorld, unitTag, send, recv)
 import Control.Parallel.MPI.Base (anySource)
-import Control.Parallel.MPI.Internal (Status)
+import Control.Parallel.MPI.Internal (Status,toRank)
+--import Control.Distributed.Process.Serializable
 import qualified Data.HashTable.IO as H
 import HashCache
 import Control.Monad
 import Data.Binary
 import Data.Typeable
 import Data.Maybe
+import GHC.Generics
 --import Tp3.Chess
-import Data.Serialize
 --0  c'est le jeu d'echec
 --1 c'est la table
 -- >1 c'est des noyaux de calcul
 
 --data MPIRequestType = GetGameResult ChessGame | SetGameResult (ChessGame,Int) deriving (Serializable)
-data MPIRequestType = GetGameResult [Char] | SetGameResult ([Char],Int) deriving (Typeable)
+{-data MPIRequestType = GetGameResult [Char] | SetGameResult ([Char],Int) deriving (Typeable, Generic)
 
 data MPIRequest = MPIRequest {
     sender :: Int,
-    requestType :: MPIRequestType
-} deriving (Typeable)
+    requestType :: (Bool, String, Int)
+} deriving (Typeable,Generic)
 instance Binary MPIRequestType
-instance Binary MPIRequest
+instance Binary MPIRequest-}
+
+--Ce que j,attend! (Int sender,(Bool isGet,String Hash du jeu,Int cost))
+--Dans le cas d'un get je me fou du cost donc mettez ce que vous voulez
 
 calculEchec echec hashTable unitTag = ()
 
@@ -45,18 +51,19 @@ main = mpiWorld $ \size rank ->
          0 -> do (msg, _status) <- recv commWorld 1 unitTag
                  putStrLn msg
          1 -> startTable size 1 unitTag
+         _ -> return()
          --_ -> calculEchec 0 1 unitTag
 sendResponse :: (Maybe Int,Int) -> IO()
-sendResponse (Nothing,sender) = return ()
+sendResponse (Nothing,_) = return ()
 sendResponse (response,sender) =
-        send commWorld  sender unitTag (fromJust response)
+        send commWorld  (toRank sender) unitTag (fromJust response)
 
-handleRequest :: H.BasicHashTable String Int -> (MPIRequest, Control.Parallel.MPI.Internal.Status) -> IO(Maybe Int,Int)
-handleRequest table ((MPIRequest sender (GetGameResult chessGame)),status) =
+handleRequest :: H.BasicHashTable String Int -> ((Int,(Bool,String,Int)), Control.Parallel.MPI.Internal.Status) -> IO(Maybe Int,Int)
+handleRequest table (( sender, (True, chessGame,_)),_) =
   do
-    value <- getValue table chessGame
-    return (value, sender)
-handleRequest table ((MPIRequest sender (SetGameResult (chessGame,value))),status) =
+    cost <- getValue table chessGame
+    return (cost, sender)
+handleRequest table (( sender, (False, chessGame,value)),_) =
     do
       insertH table (chessGame) value
       return (Nothing,sender)
