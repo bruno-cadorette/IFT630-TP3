651201d516fddc1b66dce33b80d33489c4bbc421
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2015-08-06 14:15:11 -0400

    Add Bruno's work to MPI

--- Interact.hs
+++ Interact.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE DeriveGeneric #-}
+module Interact (human,Player,ai,playGame,playTurn,printGame,computeNode') where
+
+
 import Chess
 import AlgebraicChessNotation
 import ArtificialIntelligence
 import Data.Maybe
-import qualified Data.Map as Map 
+import qualified Data.Map as Map
 import Control.Applicative
 import Control.Concurrent
 import Control.Concurrent.Async
@@ -12,7 +15,17 @@ import GHC.Generics
 import GHC.Conc (numCapabilities)
 type Player = ChessGame -> IO(Maybe ChessGame)
 
-verifyCheckMate game = 
+type SenderRank = Int
+data MPIRequestType =
+    GetCache ChessGame SenderRank |
+    SetCache (ChessGame,Int) SenderRank |
+    GetGameResult ChessGame |
+    CancelComputation deriving (Generic)
+
+
+instance Serialize MPIRequestType
+
+verifyCheckMate game =
     if isCheckMate game then
         Nothing
     else
@@ -28,13 +41,13 @@ human game = do
 
 ai :: Int -> Player
 ai depth game = do return $ verifyCheckMate $ fromJust $ play game origin target
-    where 
-        (origin, target) = getMoveInput $ minmax depth game 
-        
+    where
+        (origin, target) = getMoveInput $ minmax depth game
+
 playGame :: Player -> Player -> IO()
-playGame whitePlayer blackPlayer = 
+playGame whitePlayer blackPlayer =
     playTurn baseConfiguration $ cycle [whitePlayer, blackPlayer]
-    
+
 playTurn :: ChessGame -> [Player] -> IO ()
 playTurn game (f:fs) = do
     printGame game
@@ -46,46 +59,29 @@ playTurn game (f:fs) = do
 
 printGame :: ChessGame -> IO()
 printGame = putStrLn . prettyPrint
-        
-        
-type SenderRank = Int
-data MPIRequestType = 
-    GetCache ChessGame SenderRank | 
-    SetCache (ChessGame, ChessGame) | 
-    GetGameResult ChessGame | 
-    CancelComputation deriving (Generic)
 
-        
+
+
 start = ChessGame White (Board {boardMap = Map.fromList [((0,0),ChessPiece {pieceType = Rook, pieceColor = White}),((0,1),ChessPiece {pieceType = Pawn, pieceColor = White}),((0,2),ChessPiece {pieceType = Rook, pieceColor = White}),((0,6),ChessPiece {pieceType = Pawn, pieceColor = Black}),((0,7),ChessPiece {pieceType = Rook, pieceColor = Black}),((1,0),ChessPiece {pieceType = Knight, pieceColor = White}),((1,1),ChessPiece {pieceType = Pawn, pieceColor = White}),((1,5),ChessPiece {pieceType = Rook, pieceColor = Black}),((1,6),ChessPiece {pieceType = Pawn, pieceColor = Black}),((1,7),ChessPiece {pieceType = Knight, pieceColor = Black}),((2,0),ChessPiece {pieceType = Bishop, pieceColor = White}),((2,1),ChessPiece {pieceType = Pawn, pieceColor = White}),((2,6),ChessPiece {pieceType = Pawn, pieceColor = Black}),((2,7),ChessPiece {pieceType = Bishop, pieceColor = Black}),((3,0),ChessPiece {pieceType = Queen, pieceColor = White}),((3,1),ChessPiece {pieceType = Pawn, pieceColor = White}),((3,6),ChessPiece {pieceType = Pawn, pieceColor = Black}),((3,7),ChessPiece {pieceType = Queen, pieceColor = Black}),((4,0),ChessPiece {pieceType = King, pieceColor = White}),((4,1),ChessPiece {pieceType = Pawn, pieceColor = White}),((4,6),ChessPiece {pieceType = Pawn, pieceColor = Black}),((4,7),ChessPiece {pieceType = King, pieceColor = Black}),((5,0),ChessPiece {pieceType = Bishop, pieceColor = White}),((5,1),ChessPiece {pieceType = Pawn, pieceColor = White}),((5,6),ChessPiece {pieceType = Pawn, pieceColor = Black}),((5,7),ChessPiece {pieceType = Bishop, pieceColor = Black}),((6,0),ChessPiece {pieceType = Knight, pieceColor = White}),((6,1),ChessPiece {pieceType = Pawn, pieceColor = White}),((6,6),ChessPiece {pieceType = Pawn, pieceColor = Black}),((6,7),ChessPiece {pieceType = Knight, pieceColor = Black}),((7,3),ChessPiece {pieceType = Pawn, pieceColor = White}),((7,4),ChessPiece {pieceType = Pawn, pieceColor = Black})]})
-          
-recv :: Int -> IO (MPIRequestType, Int)
-recv 50 = return $ (CancelComputation, 0)
-recv _ = do 
-    threadDelay 5
-    return $ (GetGameResult start, 0)
-
-send ::  Int -> String -> IO ()
-send a msg = putStrLn $ "Sent " ++ msg ++ " to " ++ show a
 
 
 
-computeNode = computeNode' []
-    where
-    computeNode' flags = do
-        (msg, status) <- recv 0
-        case msg of
-            GetGameResult game -> do
-                printGame game 
-                stopFlag <- newEmptyMVar
-                forkIO $ send 0 $ minmax 4 game -- Mettre le flag dans minmax
-                computeNode' (stopFlag:flags)
-            CancelComputation -> do
-                putStrLn "Task cancelled" 
-                mapM_ (\x-> putMVar x ()) flags
-            _ -> error "Bad type MPI Request"
-                
 
-main :: IO ()
+computeNode' send rcv flags = do
+    (msg, status) <- rcv
+    case msg of
+        GetGameResult game -> do
+            printGame game
+            stopFlag <- newEmptyMVar
+            forkIO $ send $ minmax 4 game -- Mettre le flag dans minmax
+            computeNode' send rcv  (stopFlag:flags)
+        CancelComputation -> do
+            putStrLn "Task cancelled"
+            mapM_ (\x-> putMVar x ()) flags
+        _ -> error "Bad type MPI Request"
+
+
+{-main :: IO ()
 main = do
     --mapM_ print [getNotation x y|x<-[(i,j)|i<-[0..7],j<-[0..7]], y<-[(i,j)|i<-[0..7],j<-[0..7]]]
     --test baseConfiguration
@@ -94,5 +90,4 @@ main = do
     --mapM_ (mapM_ printState . transition) $ transition $ start
     computeNode
     --playGame (ai 4) (ai 4)
-    putStrLn "Fin de la partie"
-    
+    putStrLn "Fin de la partie"-}
--- MPIServer.hs
+++ MPIServer.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
@@ -11,28 +10,27 @@ import Control.Concurrent (threadDelay)
 --import Control.Distributed.Process.Serializable
 import GHC.Generics
 import qualified Data.HashTable.IO as H
-import HashCache
 import Control.Monad
 import Data.Maybe
 import Data.Serialize
 import Chess
+import Data.ByteString
+import Interact
 
 --0  c'est le jeu d'echec
 --1 c'est la table
 -- >1 c'est des noyaux de calcul
-
+type HashTable k v = H.BasicHashTable k v
 type SenderRank = Int
 data MPIRequestType =
-    GetCache ChessGame SenderRank | 
-    SetCache (ChessGame,Int) SenderRank | 
-    GetGameResult ChessGame | 
+    GetCache ChessGame SenderRank |
+    SetCache (ChessGame,Int) SenderRank |
+    GetGameResult ChessGame |
     CancelComputation deriving (Generic)
-    
-    
-instance Serialize MPIRequestType 
 
---Ce que j,attend! (Int sender,(Bool isGet,String Hash du jeu,Int cost))
---Dans le cas d'un get je me fou du cost donc mettez ce que vous voulez
+
+instance Serialize MPIRequestType
+
 
 receiver = 0
 sender2 = 1
@@ -42,39 +40,39 @@ calculEchec echec hashTable unitTag = ()
 startTable size rank unitTag =
     --Start thread pr rcv chaque rank > 1 && < size
   do
-    table <- getHashTable
+    table <- H.new
 
-    (print "Waiting")
     (request,status) <- recv commWorld anySource unitTag
-    (putStrLn "We got a msg")
     sendResponse =<< handleRequest table (request,status)
 
 
 main :: IO ()
 main = mpiWorld $ \size rank ->
    if size < 2
-      then putStrLn "At least three processes are needed"
+      then print "At least three processes are needed"
       else case rank of
          1 -> do
                      threadDelay 500
                      print "Sending"
-                     send commWorld receiver unitTag (MPIRequest (fromRank sender2) (GetGameResult "abc"))
+                     --send commWorld receiver unitTag (GetCache ChessGame sender2)
                      print "Sent"
          0 -> startTable size receiver unitTag
-         _ -> return()
+         _ -> computeNode
          --_ -> calculEchec 0 1 unitTag
 sendResponse :: (Maybe Int,Int) -> IO()
 sendResponse (Nothing,_) = return ()
 sendResponse (response,sender) =
         send commWorld  (toRank sender) unitTag (fromJust response)
 
-handleRequest :: H.BasicHashTable String Int -> (MPIRequest, Control.Parallel.MPI.Internal.Status) -> IO(Maybe Int,Int)
-handleRequest table ((MPIRequest sender (GetGameResult chessGame)),_) =
+handleRequest :: H.BasicHashTable ByteString Int -> (MPIRequestType, Control.Parallel.MPI.Internal.Status) -> IO(Maybe Int,Int)
+handleRequest table ((GetCache chessGame sender), _) =
   do
-    cost <- getValue table chessGame
+    cost <- H.lookup table (encode chessGame)
     return (cost, sender)
-handleRequest table ((MPIRequest sender (SetGameResult (chessGame,value))),_) =
+handleRequest table ((SetCache (chessGame,value) sender), _) =
     do
-      insertH table (chessGame) value
+      H.insert table (encode chessGame) value
       return (Nothing,sender)
-      
+
+computeNode = computeNode' (send commWorld  (toRank 0) unitTag) receive  []
+            where receive = recv commWorld (toRank 0) unitTag
